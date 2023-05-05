0dd7a8a51fb27df5ed1efb29f0598cee9327c79e
Ilan Tervil <ilan.tervil@wanadoo.fr>
2023-05-05 15:04:03 +0200

    6e commit listform coponent added

---
+++ src/Component/ListForm.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Input } from "antd";
+
+const ListForm = (props) => {
+
+    console.log(props.name, props.color)
+    function handleChange(event) {
+        switch (event.target.name) {
+        case "name":
+                props.setName(event.target.value);
+            break; 
+        case "color":
+                props.setColor(event.target.value);
+                break;
+        default:
+            break;
+    }
+}
+
+return (
+    <form>
+        <label htmlFor="listName">Nom de la liste</label>
+        <Input placeholder='Nom de la liste' onChange={handleChange} value= {props.name} name='name' id='name'/>
+        <label htmlFor="color">Couleur</label>
+        <Input type="color" name='color' id='color' onChange={handleChange} value={props.color} />
+    </form>
+        )  
+}
+
+export default ListForm
--- src/Component/ListModal.jsx
+++ src/Component/ListModal.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import { Modal } from "antd";
+import ListForm from './ListForm';
+import { useState } from 'react';
+
 
 const ListModal = (props) => {
+    const [name, setName] = useState("");
+    const [color, setColor] = useState("#3B97FF");
+
     return (
         <>
-            <Modal title={props.modalTitle} visible={props.isModalOpen} onCancel={props.handleCancel}>
-                <p>Some contents...</p>
-                <p>Some contents...</p>
-                <p>Some contents...</p>
+            <Modal 
+            title={props.modalTitle} 
+            visible={props.isModalOpen} 
+            onCancel={props.handleCancel}>
+
+            <ListForm name={name} color={color} setName={setName} setColor={setColor}/>
+
             </Modal>
         </>
     )
