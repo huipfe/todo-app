bc62ce7e5658088a5f099b32ef4d08524c58bcd1
Ilan Tervil <ilan.tervil@wanadoo.fr>
2023-05-05 18:08:27 +0200

    7e commit Init Firebase

--- src/Component/ListModal.jsx
+++ src/Component/ListModal.jsx
@@ -1,20 +1,45 @@
 import React from 'react'
 import { Modal } from "antd";
+import { Button } from "antd";
 import ListForm from './ListForm';
 import { useState } from 'react';
-
+import Fire from '../Fire';
 
 const ListModal = (props) => {
     const [name, setName] = useState("");
     const [color, setColor] = useState("#3B97FF");
+    const [error, setError] = useState([]);
+
+    function handleSubmit() {
+        const firebase = new Fire ((error) => { 
+            if (error) {
+                setError(error);
+            }else {
+                const list = {
+                    name: name,
+                    color: color,
+                    tasks: [],
+                }
+
+                firebase.addList(list);
+            }
+        });
+        props.handleCancel();
+    }
 
     return (
         <>
             <Modal 
             title={props.modalTitle} 
             visible={props.isModalOpen} 
-            onCancel={props.handleCancel}>
-
+            onCancel={props.handleCancel}
+            footer={[
+                <Button type="primary"  onClick={handleSubmit}>
+                    Créer
+                </Button>
+            ]}
+            >
+            {error && <p>{error}</p>}
             <ListForm name={name} color={color} setName={setName} setColor={setColor}/>
 
             </Modal>
---
+++ src/Fire.js
@@ -0,0 +1,96 @@
+// Import the functions you need from the SDKs you need
+
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+// import firebaseConfig from "./firebaseConfig";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBdaM6wVgFTOG6NgOQ_lEqyYR3JruiufaE",
+    authDomain: "todo-app-2111e.firebaseapp.com",
+    projectId: "todo-app-2111e",
+    storageBucket: "todo-app-2111e.appspot.com",
+    messagingSenderId: "476407362265",
+    appId: "1:476407362265:web:7273aca2a48aaf3e0404c8",
+    measurementId: "G-0H8MRF5ML9"
+};
+
+// Initialize Firebase
+
+export default class Fire {
+    constructor(callback) {
+        if (!firebase.apps.length) {
+            firebase.initializeApp(firebaseConfig);
+        }
+
+        firebase.auth().onAuthStateChanged(user => {
+            if (user) {
+                callback(null);
+            } else {
+                firebase.auth().signInAnonymously().catch(error => {
+                    callback(error);
+                });
+            }
+        });
+    }
+
+    get ref() {
+        return firebase.database().collection("Lists");
+    }
+
+    getLists(callback) {
+        let ref = this.ref.orderBy("name");
+        this.unsubscribe = ref.onSnapshot(snapshot => {
+            let lists = [];
+            snapshot.forEach(doc => {
+                lists.push({ id: doc.id, ...doc.data() });
+            });
+            callback(lists);
+        }, function (error) {
+            callback(error);
+        });
+    }
+
+    addList(list) {
+        this.ref.add(list);
+    }
+
+    deleteList(list) {
+        this.ref.doc(list.id).delete();
+    }
+
+    updateList(list) {
+        this.ref.doc(list.id).update(list);
+    }
+
+    detach() {
+        this.unsubscribe();
+    }
+}
+
+constructor(callback) {
+    if (!app) {
+        app = initializeApp(firebaseConfig);
+        analytics = getAnalytics(app);
+    }
+
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            callback(null);
+        } else {
+            firebase
+                .auth()
+                .signInAnonymously()
+                .catch((error) => {
+                    callback(error);
+                });
+        }
+    });
+}
